027ef3513037cfce6b14597d23851c1c5aa8d352
ABDUMAJIDOV OZODBEK <135005963+abdumajidovOzodbek@users.noreply.github.com>
2025-03-25 06:52:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { exec } = require("child_process");
 const ffmpegPath = require("ffmpeg-static");
 const path=require("path");
-const videoUrl = "https://s3-hzde.freeconvert.com/task/67e2072fec48b99fdffbf681/0325.mp4"
+const videoUrl = path.resolve(__dirname, "short.mp4")
 const youtubeStreamUrl =
     "rtmp://a.rtmp.youtube.com/live2/6m24-pfdk-jhwq-1krb-3ufz"; // Replace with your actual stream key
 
 const command = `${ffmpegPath} -stream_loop -1 -re -i "${videoUrl}" \
--c:v libx264 -b:v 3500k -preset veryfast -tune zerolatency -g 60 -r 30 -s 1080x1920 \
+-c:v libx264 -b:v 3000k -preset veryfast -tune zerolatency -g 60 -r 30 -s 1080x1920 \
 -c:a aac -b:a 192k -ar 48000 -f flv "${youtubeStreamUrl}"`;
 
 exec(command, (error, stdout, stderr) => {
